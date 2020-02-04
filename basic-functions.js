7dd071fbe3a40c0dd37e46626293408276fbe42f
Phil Hawker <philhawker1211@gmail.com>
2020-02-03 21:45:14 -0700

    inital commit

--- basic-functions.js
+++ basic-functions.js
@@ -19,8 +19,8 @@ console.log(stringChecker(numOne))
 console.log(stringChecker(numTwo))
 console.log(stringChecker('hi there'))
 
+
 // #Write a JavaScript function to capitalize the first letter of a string or argument.
-// - ex: capitalizeString( “hi there” ); // returns “Hi there”
 
 function capitalizeMe(i) {
     i = i.charAt(0).toUpperCase() + i.slice(1)
@@ -30,8 +30,8 @@ function capitalizeMe(i) {
 console.log(capitalizeMe(word))
 console.log(capitalizeMe(sentence))
 
+
 // # Write a JavaScript function that accept two integers as arguments and returns the larger number.
-// - ex: largerNumber( 12, 9 ); // returns 12
 
 function largerNum(num1, num2) {
     if (num1 > num2) {
@@ -45,8 +45,8 @@ console.log(largerNum(numOne, numTwo))
 console.log(largerNum(88, 100))
 console.log(largerNum(88, 3))
 
+
 // # Write a javascript function that adds two strings together
-//     - ex: stringAdder( “hi”, “there” ); // returns “hi there”
 
 const stringOne = 'Hello sir.'
 const stringTwo = 'How are you today?'
