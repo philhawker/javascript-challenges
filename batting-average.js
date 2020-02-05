32d7ebc9a057cd4e32636fff0ebb69cac364915a
Phil Hawker <philhawker1211@gmail.com>
2020-02-05 09:47:12 -0700

    inital commit'

--- batting-average.js
+++ batting-average.js
@@ -3,7 +3,7 @@ function battingAverage () {
     var atBats = 300;
 
     return {
-        gertCurrentAverage: function () {     // This is the "closure". Notice the use of an expression when creating a function within a function
+        getCurrentAverage: function () {     // This is the "closure". a function in a function. Notice the use of an expression when creating a function within a function
             return (hits/atBats)
         },
         updateHitsAndAtBats: function (hit, atBat) {      // This return is just an object (ditctionary), a key value pair with {}
@@ -15,6 +15,6 @@ function battingAverage () {
 
 
 var altuve = battingAverage()
-console.log(altuve.gertCurrentAverage())
+console.log(altuve.getCurrentAverage())
 altuve.updateHitsAndAtBats(0, 20)
 console.log(altuve.gertCurrentAverage())
