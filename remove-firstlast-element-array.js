8eb943f73f0bc69fd7b12e899dfc5fdfd4a2a4c2
Phil Hawker <philhawker1211@gmail.com>
2020-02-17 14:01:31 -0700

    changed function to include if loop

--- remove-firstlast-element-array.js
+++ remove-firstlast-element-array.js
@@ -3,10 +3,14 @@
 
 let arr = [  'rgdgbfdbfd', 'keep', 'keep', 'ergergv']
 
-function removeFirstLast(arr) {
-    arr.pop()
-    arr.shift()
-        return arr.toString().trim()
+const removeFirstLast = (arr) => {
+    if (arr.length >= 3) {
+        return arr.slice(1, -1)
+    } else {
+        throw new Error('You need at least three elements in the array')
+    }
 }
 
+console.log(removeFirstLast([1, 2, 3, 4, 5]))
 console.log(removeFirstLast(arr))
+console.log(removeFirstLast([1, 2]))
